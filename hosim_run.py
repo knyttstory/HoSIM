a6af0e8d21e40c2db1ae4d91a16c3ed779c58da1
knyttstory <87790601+knyttstory@users.noreply.github.com>
2022-02-10 16:10:42 +0800

    Update hosim_run.py

--- hosim_run.py
+++ hosim_run.py
@@ -4,7 +4,6 @@ import tqdm
 import pickle
 import networkx as nx
 
-import preprocess_data
 from data_setting import *
 from parameter_setting import *
 from sample_subgraph import *
@@ -29,51 +28,60 @@ def read_diffusion_information():
     return diffused_nodes, coefficient_nodes, diffusion_path
 
 def sample_all_subgraphs(graph_data, diffused_nodes, coefficient_nodes):
-    sample_path = "ds%d_lf%s_ls%d_in%d_af%s" % (diffusion_size, "T" if lorw_flag else "F", lorw_size, iters_number, "T" if add_boundary_flag else "F")
+    sample_path = "ds%d_lf%s_ls%d_in%d_af%s" % (diffusion_size, "T" if lorw_flag else "F", lorw_size, iters_number, str(add_boundary_number) if add_boundary_number is not None else "F")
     os.makedirs(os.path.join(output_path, dataset_name, sample_path), exist_ok=True)
     if os.path.exists(os.path.join(output_path, dataset_name, sample_path, output_sampled_graph)):
         with open(os.path.join(output_path, dataset_name, sample_path, output_sampled_graph), 'rb') as sf:
             sampled_all_subgraphs, sampled_all_cores = pickle.load(sf)
+        sample_time = 0
     else:
         sampled_all_subgraphs = dict()
         sampled_all_cores = dict()
         print("Start to sample nodes")
+        start_time = time.time()
+        sum_nodes = 0
         for i in range(len(dataset_seeds)):
             with open(os.path.join(dataset_path, dataset_name, dataset_seeds[i]), 'rb') as sf:
                 query_nodes = pickle.load(sf)
+                sum_nodes += len(query_nodes)
             for qn in tqdm.tqdm(query_nodes):
                 sampled_all_subgraphs[qn], sampled_all_cores[qn] = sample_subgraph(graph_data, qn, diffused_nodes, coefficient_nodes)
+        sample_time = time.time() - start_time
+        print("Average sample time: %.2f" % (sample_time / sum_nodes))
         with open(os.path.join(output_path, dataset_name, sample_path, output_sampled_graph), 'wb') as sf:
             pickle.dump((sampled_all_subgraphs, sampled_all_cores), sf)
-    return sampled_all_subgraphs, sampled_all_cores, sample_path
+    return sampled_all_subgraphs, sampled_all_cores, sample_path, sample_time
 
 
 def main():
     graph_data = read_graph_data()
     diffused_nodes, coefficient_nodes, diffusion_path = read_diffusion_information()
-    sampled_all_subgraphs, sampled_all_cores, sample_path = sample_all_subgraphs(graph_data, diffused_nodes, coefficient_nodes)
+    sampled_all_subgraphs, sampled_all_cores, sample_path, sample_time = sample_all_subgraphs(graph_data, diffused_nodes, coefficient_nodes)
     detect_path = "sf%s_cn%d_at%.1f_rf%s_rt%.1f" % ("T" if speedup_flag else "F", components_number, add_threshold, "T" if remove_flag else "F", remove_threshold)
     os.makedirs(os.path.join(output_path, dataset_name, sample_path, detect_path), exist_ok=True)
     print("Start to detect communities")
+    detect_time = 0
     sum_nodes = 0
     for i in range(len(dataset_seeds)):
         with open(os.path.join(dataset_path, dataset_name, dataset_seeds[i]), 'rb') as sf:
             query_nodes = pickle.load(sf)
-        sum_nodes += len(query_nodes)
+            sum_nodes += len(query_nodes)
         detected_communities = dict()
+        start_time = time.time()
         for qn in tqdm.tqdm(query_nodes):
             subgraph_data = nx.subgraph(graph_data, sampled_all_subgraphs[qn])
             detected_communities[qn] = detect_multiple_communities(graph_data, subgraph_data, sampled_all_cores[qn], qn, diffused_nodes, coefficient_nodes)
+        detect_time += time.time() - start_time
         with open(os.path.join(output_path, dataset_name, sample_path, detect_path, output_detected_community[i]), 'wb') as df:
             pickle.dump(detected_communities, df)
+    print("Average detection time: %.2f" % (detect_time / sum_nodes))
     with open(os.path.join(output_path, dataset_name, diffusion_path), 'wb') as df:
         pickle.dump((diffused_nodes, coefficient_nodes), df)
-    return sample_path, detect_path, sum_nodes
+    print("Average time: %.2f" % ((sample_time + detect_time) / sum_nodes))
+    return sample_path, detect_path
+
 
 if __name__ == '__main__':
     print(dataset_name)
-    preprocess_data.main(dataset_name)
-    start_time = time.time()
-    sample_path, detect_path, sum_nodes = main()
-    print(round(time.time() - start_time, 2) / sum_nodes)
+    sample_path, detect_path = main()
     evaluate_community.main_run(sample_path, detect_path)

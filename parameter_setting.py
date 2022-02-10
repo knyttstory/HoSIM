1740be42b289a0ed8b1c70c094802ceff51cfffd
knyttstory <87790601+knyttstory@users.noreply.github.com>
2022-02-10 16:11:01 +0800

    Update parameter_setting.py

--- parameter_setting.py
+++ parameter_setting.py
@@ -1,15 +1,18 @@
 maximum_neighbor_number = 10
 pr_steps = 4
 pr_hop = 2
+maximum_diffusion_subgraph_size = 100
 
 diffusion_size = 100
 lorw_flag = True
 lorw_size = 100
 iters_number = 10
-add_boundary_flag = True
+add_boundary_number = 2
+all_shortest_flag = True
+maximum_sample_subgraph_size = 10000
 
 speedup_flag = True
 components_number = 10
-add_threshold = 0.7
+add_threshold = 0.3
 remove_flag = True
-remove_threshold = 0.3
+remove_threshold = 0.2

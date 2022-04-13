1778373b453470cdfcf625d53545b267267d553f
knyttstory <87790601+knyttstory@users.noreply.github.com>
2022-04-13 17:59:48 +0800

    Update calculate_importance.py

--- calculate_importance.py
+++ calculate_importance.py
@@ -1,22 +1,20 @@
 import copy
 import numpy as np
-import networkx as nx
 
 from parameter_setting import *
+import utils
 
-def diffuse_node(graph_data, dn, diffused_nodes, coefficient_nodes):
-    if dn in diffused_nodes:
-        return
-    else:
-        subgraph_nodes = sample_diffuse_nodes(graph_data, dn, coefficient_nodes)
-        temp_subgraph = nx.subgraph(graph_data, subgraph_nodes)
-        sorted_nodes = sorted(list(temp_subgraph.nodes))
+
+def diffuse_node(adjacency_list, dn, diffused_nodes, coefficient_nodes):
+    if dn not in diffused_nodes:
+        subgraph_nodes = sample_diffuse_nodes(adjacency_list, dn, coefficient_nodes)
+        sorted_nodes = sorted(list(subgraph_nodes))
         node_index = dict()
         for si, sn in enumerate(sorted_nodes):
             node_index[sn] = si
         seed_index = node_index[dn]
-        A = nx.to_numpy_array(temp_subgraph, nodelist=sorted_nodes, dtype=np.float16)
-        inverse_degree_array = [1/nx.degree(temp_subgraph, tn) for tn in sorted_nodes]
+        A = generate_adjacency_matrix(adjacency_list, sorted_nodes)
+        inverse_degree_array = [1/len(adjacency_list[tn].intersection(subgraph_nodes)) for tn in sorted_nodes]
         inverse_D = np.diag(inverse_degree_array)
         transition_matrix = np.dot(inverse_D, A)
         probability_vector = np.zeros((1, len(sorted_nodes)))
@@ -29,9 +27,9 @@ def diffuse_node(graph_data, dn, diffused_nodes, coefficient_nodes):
         diffused_nodes[dn] = dict()
         for si, sn in enumerate(sorted_nodes):
             diffused_nodes[dn][sn] = probability_vector[0, si]
-        return
+    return
 
-def sample_diffuse_nodes(graph_data, start_node, coefficient_nodes):
+def sample_diffuse_nodes(adjacency_list, start_node, coefficient_nodes):
     subgraph_nodes = set()
     subgraph_nodes.add(start_node)
     seed_neighbors = set()
@@ -39,21 +37,29 @@ def sample_diffuse_nodes(graph_data, start_node, coefficient_nodes):
     for i in range(pr_hop):
         new_neighbors = set()
         for sn in seed_neighbors:
-            temp_neighbors = set(pick_nodes_by_coefficient(graph_data, sn, coefficient_nodes))
+            temp_neighbors = set(pick_nodes_by_coefficient(adjacency_list, sn, coefficient_nodes))
             subgraph_nodes = subgraph_nodes.union(temp_neighbors)
             new_neighbors = new_neighbors.union(temp_neighbors)
         seed_neighbors = copy.deepcopy(new_neighbors)
     return subgraph_nodes
 
-def pick_nodes_by_coefficient(graph_data, center_node, coefficient_nodes):
-    if maximum_neighbor_number is not None and len(list(nx.neighbors(graph_data, center_node))) > maximum_neighbor_number:
+def pick_nodes_by_coefficient(adjacency_list, center_node, coefficient_nodes):
+    if maximum_neighbor_number > 0 and len(adjacency_list[center_node]) > maximum_neighbor_number:
         nodes_coefficients = list()
-        for tn in nx.neighbors(graph_data, center_node):
+        for tn in adjacency_list[center_node]:
             if tn not in coefficient_nodes:
-                coefficient_nodes[tn] = nx.clustering(graph_data, tn)
+                coefficient_nodes[tn] = utils.calculate_clustering(adjacency_list, tn)
             nodes_coefficients.append([tn, coefficient_nodes[tn]])
         nodes_coefficients = sorted(nodes_coefficients, key=lambda x: x[1], reverse=True)
-        nodes_coefficients = nodes_coefficients[:maximum_neighbor_number]
-        return [tn[0] for tn in nodes_coefficients]
+        node_set = set([tn[0] for tn in nodes_coefficients[:maximum_neighbor_number]])
+        return node_set
     else:
-        return list(nx.neighbors(graph_data, center_node))
+        return adjacency_list[center_node]
+
+def generate_adjacency_matrix(adjacency_list, sorted_nodes):
+    A = np.zeros((len(sorted_nodes), len(sorted_nodes))).astype(np.float16)
+    for i, u in enumerate(sorted_nodes):
+        for j, v in enumerate(sorted_nodes):
+            if v in adjacency_list[u]:
+                A[i][j] = 1
+    return A
